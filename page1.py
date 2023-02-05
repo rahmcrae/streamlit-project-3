a751a844a2f538edb1060288e5c606c1450fe1c4
Rah <rah@consult-rah.com>
2023-02-04 21:35:23 -0500

    fix max_value

--- page1.py
+++ page1.py
@@ -21,7 +21,7 @@ def show():
     plot_data = st.selectbox("select the data to plot", ["Open", "High", "Low", "Close","Volume"])
     plot_data_select = str(plot_data)
     
-    max_value = len(pd.DataFrame(yf.download(ticker, start=start_date, end=date.today(), group_by='ticker')))
+    max_value = len(pd.DataFrame(yf.download(ticker, start=start_date, end=date.today(), group_by='ticker')).reset_index())
     # Select the # of rows for table
     number = st.slider("select the number of records for table",  min_value=None, max_value=max_value, value=None, step=10,label_visibility="visible")    
     
