6f11ddb6fbc1667e19a987fab4c6d4c360d247ba
Rah <rah@consult-rah.com>
2023-02-04 21:44:37 -0500

    fix max_value

--- page2.py
+++ page2.py
@@ -2,7 +2,7 @@ import streamlit as st
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
-import page1
+from page1 import *
 
 def show():
     st.write("This is Page 2")
